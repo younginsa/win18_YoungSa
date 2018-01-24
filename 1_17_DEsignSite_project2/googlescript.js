7a3821fbac68a34aa25c2e0920193de0c85d235b
younginsa <wow0726@gmail.com>
2018-01-24 09:22:14 -0500

    compilation with project2 update

--- 1_17_DEsignSite_project2/googlescript.js
+++ 1_17_DEsignSite_project2/googlescript.js
@@ -6,10 +6,8 @@ $(document).ready(function(){
     $('.list').append('<div class="item">'+sketchItem+'</div>');
   });
 
-  $(document).on('dblclick','.item',function(){
-      $(this).remove();
-  });
-  
+
+/*
     $(document).on('click','.item',function(){
       var searchIt = $(this).text();
       console.log('searchIt')
@@ -17,13 +15,54 @@ $(document).ready(function(){
       $(this).addClass('donelist');
     });
 
+    $(document).on('dblclick','.item',function(){
+        $(this).remove();
+    });
 
+    function doClickAction() {
+      $("#click h3").append("•");
+    }
+    function doDoubleClickAction() {
+      $("#double-click h3").append("•");
+    }
+
+*/
+
+//-----------
+
+var timer = 0;
+var delay = 200;
+var prevent = false;
+
+$(document)
+  .on('click','.item',function() {
+    var searchIt = $(this).text();
+    console.log(searchIt)
+    $(this).addClass('donelist');
+    timer = setTimeout(function() {
+      if (!prevent) {
+        myFunction(searchIt);
+        doClickAction();
+      }
+      prevent = false;
+    }, delay);
+  })
+  .on('dblclick','.item', function() {
+    $(this).remove();
+    clearTimeout(timer);
+    prevent = true;
+    doDoubleClickAction();
+  });
+
+
+
+//------------
 
   });
 
 
   function myFunction(searchIt) {
 
-      var myWindow = window.open("https://www.google.com/search?q="+searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=400");
+      var myWindow = window.open("https://www.google.com/search?q="+ searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=400");
 
   }
